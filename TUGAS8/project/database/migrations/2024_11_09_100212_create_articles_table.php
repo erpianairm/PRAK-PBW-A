11c68af813669dbeacb8b70701e68a7a105125ad
erpianairm <irmaerpianaaa@gmail.com>
2024-11-09 21:10:39 +0700

    TUGAS8

---
+++ TUGAS8/project/database/migrations/2024_11_09_100212_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('title'); //'Apa yang terbaru di laravel 11?'
+            $table->string('slug')->unique(); //'apa-yang-terbaru-di-laravel-11-?'
+            $table->text('teaser');
+            $table->text('body');
+            $table->string('meta_title');
+            $table->string('meta_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+};
---
+++ TUGAS8/project/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', Controllers\HomeController::class);
+
+
+Route::get('/about', [Controllers\AboutController::class, 'index']);
+
+Route::get('/contact', [Controllers\ContactController::class, 'index']);
+
+Route::get('/gallery', [Controllers\GalleryController::class, 'index']);
+
+
+
